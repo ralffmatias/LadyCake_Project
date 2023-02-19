48470fe2a59cf59c193b35f0ae4d756ace3f1013
Ralff Matias <114631403+ralffmatias@users.noreply.github.com>
2023-02-19 19:16:36 -0300

    Update Inserir_Lady_Cake.sql

--- SQLCode/Inserir_Lady_Cake.sql
+++ SQLCode/Inserir_Lady_Cake.sql
@@ -7,38 +7,33 @@ INSERT INTO medidas  VALUES ('2Kg', '20cm X 10cm', 20, 100);
 INSERT INTO medidas  VALUES ('3Kg', '26cm X 10cm', 25, 150);
 INSERT INTO medidas  VALUES ('4Kg', '30cm X 10cm', 30, 200);
 
-
 INSERT INTO tradicional_adicional VALUES (0, 00)
 INSERT INTO tradicional_adicional VALUES (1, 10)
 
-
 INSERT INTO recheios  VALUES ('Brigadeiro', 1);
 INSERT INTO recheios  VALUES ('Beijinho', 1);
-INSERT INTO recheios  VALUES ('Prestígio', 1);
+INSERT INTO recheios  VALUES ('PrestÃ­gio', 1);
 INSERT INTO recheios  VALUES ('Doce de leite', 1);
 INSERT INTO recheios  VALUES ('Brigadeiro branco', 1);
 INSERT INTO recheios  VALUES ('Mousse de brigadeiro', 1);
 INSERT INTO recheios  VALUES ('Brigadeiro de ninho', 2);
 INSERT INTO recheios  VALUES ('Mousse de ninho', 2);
-INSERT INTO recheios  VALUES ('Sensação', 2);
+INSERT INTO recheios  VALUES ('SensaÃ§Ã£o', 2);
 INSERT INTO recheios  VALUES ('Ninho com frutas', 2);
 INSERT INTO recheios  VALUES ('Ninho com coco', 2);
 INSERT INTO recheios  VALUES ('Brigadeiro com frutas', 2);
 INSERT INTO recheios  VALUES ('Coco com abacaxi', 2);
 
-
-INSERT INTO massas VALUES ('Pão de ló');
+INSERT INTO massas VALUES ('PÃ£o de lÃ³');
 INSERT INTO massas VALUES ('Chocolate');
 INSERT INTO massas VALUES ('Red velvet');
 INSERT INTO massas VALUES ('Amanteigada');
 
-
 INSERT INTO topper VALUES (0, 00);
 INSERT INTO topper VALUES (1, 15);
 
-
-INSERT INTO clientes VALUES ('Lucas Brito', NULL, 61, '999313511');
-INSERT INTO clientes VALUES ('Odileia Sena', NULL, 61, '999281084');
-INSERT INTO clientes VALUES ('Cida Sena', NULL, 61, '998346833');
-INSERT INTO clientes VALUES ('Simone Bito', NULL, 61, '995687898');
+INSERT INTO clientes VALUES ('Cliente 1', NULL, 61, '999999991');
+INSERT INTO clientes VALUES ('Cliente 2', NULL, 61, '999999992');
+INSERT INTO clientes VALUES ('Cliente 3', NULL, 61, '999999993');
+INSERT INTO clientes VALUES ('Cliente 4', NULL, 61, '999999994');
 GO
